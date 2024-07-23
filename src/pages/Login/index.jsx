47c9bb52bb319ac93ed87eab0e35a342262e2b1f
추성우 <cntjddn715@gmail.com>
2024-07-23 21:02:11 +0900

    로그인 퍼블리싱

--- src/pages/Login/index.jsx
+++ src/pages/Login/index.jsx
@@ -1,11 +1,38 @@
-import React from "react";
+import React, { useState } from "react";
 import * as S from "./style";
 import Header from "../../components/Header/Header";
 
 const Login = () => {
+  const [inputs, setInputs] = useState({ id: "", password: "" });
+
+  const handleInputs = (e) => {
+    setInputs({
+      ...inputs,
+      [e.target.name]: e.target.value,
+    });
+  };
   return (
     <S.Layout>
       <Header />
+      <S.Content>
+        <S.LoginBox>
+          <S.Title>로그인</S.Title>
+          <S.Input
+            name="id"
+            value={inputs.id}
+            placeholder="아이디를 입력하세요."
+            onChange={handleInputs}
+          />
+          <S.Input
+            name="password"
+            value={inputs.password}
+            type="password"
+            placeholder="비밀번호를 입력하세요."
+            onChange={handleInputs}
+          />
+          <S.Button>로그인</S.Button>
+        </S.LoginBox>
+      </S.Content>
     </S.Layout>
   );
 };
