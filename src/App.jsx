f992cfb6cde422938a54889c23968e7c2016a9fd
추성우 <cntjddn715@gmail.com>
2024-07-23 22:05:32 +0900

    메인 페이지 퍼블리

--- src/App.jsx
+++ src/App.jsx
@@ -3,10 +3,12 @@ import "./App.css";
 import Login from "./pages/Login";
 import Register from "./pages/Register";
 import Ticketing from "./pages/Ticketing";
+import Home from "./pages/Home";
 
 function App() {
   return (
     <Routes>
+      <Route path="/" element={<Home />} />
       <Route path="/login" element={<Login />} />
       <Route path="/register" element={<Register />} />
       <Route path="/ticketing" element={<Ticketing />} />
--- src/pages/Home/style.js
+++ src/pages/Home/style.js
@@ -6,3 +6,43 @@ export const Layout = styled.div`
   display: flex;
   flex-direction: column;
 `;
+
+export const Content = styled.div`
+  padding: 80px 150px;
+`;
+
+export const DayOfWeek = styled.div`
+  display: flex;
+  justify-content: space-between;
+`;
+
+export const Day = styled.div`
+  padding: 10px 55px;
+  cursor: pointer;
+  background-color: ${(props) => (props.isSelect ? "#4f9cd4" : "#d9d9d9")};
+  font-size: 28px;
+`;
+
+export const Movies = styled.div`
+  display: flex;
+  flex-wrap: wrap;
+  justify-content: space-between;
+`;
+
+export const Movie = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  gap: 10px;
+  margin-top: 30px;
+`;
+
+export const MovieImg = styled.img`
+  width: 180px;
+  height: 260px;
+`;
+
+export const MovieTitle = styled.div`
+  font-size: 28px;
+`;
